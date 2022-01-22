cfac0b88388fc346b8b038e79a1f7d92d9e2c4ea
pallak02 <nikkipalak2003@gmail.com>
2022-01-22 18:47:01 +0530

    styled the landing page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import data from "../data.json"
+import Button from "react-bootstrap/Button";
+function Sidebar() {
+    return (
+        <div className="sidebar">
 
-function Sidebar(){
-    return(
-        <div className="sidebar"> 
             <p className="name">hello {data.student[0].name}</p>
-            <div>
-            <Link className="link" to={"/studentDashboard/courses"}>Courses</Link>
-            </div>
-            <br/>
-            <div>
-              <Link className="link" to={"/studentDashboard/cart"}>Cart</Link>
-            </div>
             
+           
+            
+            
+
         </div>
     )
 }
